ac65c4cc7a0b016f265319a553ad1247e9740b9b
Phil Robinson <philip.robinson84@gmail.com>
2015-02-24 12:43:12 +0000

    Timelapse script now running with basic functionality

--- cam_timeLapse.py
+++ cam_timeLapse.py
@@ -1,18 +1,42 @@
 import time
 import picamera
+import os
 from subprocess import call
 
-VIDEO_DAYS = 2
+VIDEO_DAYS = 1
 FRAMES_PER_HOUR = 120
-FRAMES = FRAMES_PER_HOUR * 24 * VIDEO_DAYS
+HOURS_IN_DAY = 24
+FRAMES = FRAMES_PER_HOUR * HOURS_IN_DAY * VIDEO_DAYS
+DIRNAME = "/home/pi/timelapse"
+
+def create_dir():
+    TIME = time.localtime()
+    CURRENT_YEAR = TIME[0]
+    CURRENT_MONTH = TIME[1]
+    CURRENT_DAY = TIME[2]
+    CURRENT_HOUR = TIME[3]
+
+    print "CURRENT_YEAR = %d" % TIME[0]
+    print "CURRENT_MONTH = %d" % TIME[1]
+    print "CURRENT_DAY = %d" % TIME[2]
+    print "CURRENT_HOUR = %d" % TIME[3]
+
+    global DIRNAME
+    DIRNAME = "/home/pi/timelapse/%s%s%s_%s/" % (CURRENT_YEAR, CURRENT_MONTH, CURRENT_DAY, CURRENT_HOUR)
+    print "DIRNAME = %s" % DIRNAME
+    if not os.path.isdir(DIRNAME):
+        os.makedirs(DIRNAME)
+        print "Created folder: %s" % DIRNAME 
 
 def capture_frame(frame):
     with picamera.PiCamera() as cam:
-        time.sleep(2)
+        #time.sleep(2)
         cam.resolution = (1280, 720)
-        dirname = time.strftime("%Y%m%d")
-        cam.capture('/home/pi/timelapse/%s_frame%03d.jpg' % (dirname, frame),format='jpeg',quality=75)
-        photofile = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/timelapse/%s_frame%03d.jpg %s_frame%03d.jpg" % (dirname, frame, dirname, frame)
+        create_dir()
+        cam.capture('%sframe%03d.jpg' % (DIRNAME, frame),format='jpeg',quality=75)
+        photofile = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload %sframe%03d.jpg %sframe%03d.jpg" % (DIRNAME, frame, DIRNAME, frame)
+        call ([photofile], shell=True)
+        photofile = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload %sframe%03d.jpg latest.jpg" % (DIRNAME, frame)
         call ([photofile], shell=True)
 
 # Capture the images
