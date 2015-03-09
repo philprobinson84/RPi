7e4594f262a9a70e65ca4cd4985df1c789b3cbc0
Phil Robinson <philip.robinson84@gmail.com>
2015-03-09 08:25:49 +0000

    Added transmission to raspbmc setup

--- tools/raspbmc/setup.sh
+++ tools/raspbmc/setup.sh
@@ -1,1 +1,12 @@
-thththr
+#!/bin/bash
+
+# change to home dir
+cd /home/pi
+
+# install transmission
+sudo apt-get -y install transmission-daemon
+
+# stop transmission service
+sudo service transmission-daemon stop
+
+# copy over our config file
