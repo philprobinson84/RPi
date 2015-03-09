6f4a5fb8a18810b4a0e2cc912527aafa9250fb58
Phil Robinson <philip.robinson84@gmail.com>
2015-03-09 08:41:14 +0000

    Transmission setup should be working

--- tools/raspbmc/setup.sh
+++ tools/raspbmc/setup.sh
@@ -10,3 +10,10 @@ sudo apt-get -y install transmission-daemon
 sudo service transmission-daemon stop
 
 # copy over our config file
+sudo cp /home/pi/RPi/tools/raspbmc/settings.json /etc/transmission-daemon/settings.json
+
+# change the service config so it's run as root
+sudo cp /home/pi/RPi/tools/raspbmc/transmission-daemon /etc/init.d/transmission-daemon
+
+# restart the service
+sudo service transmission-daemon start
